e368b828a7ee62a3d7e4b51e8b22109bdc2721c5
vsouhrada <v.souhrada@gmail.com>
2017-02-25 07:52:34 +0100

    Code improvement

---
+++ app/src/main/java/com/example/android/anko/sample/sign_in/SignInActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android.anko.sample.sign_in
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SignInActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+  }
+}
